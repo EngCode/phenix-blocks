fe16cef8d0d9854867df11267468a82df94fcc93
EngCode <elampratorabdallah14@gmail.com>
2022-08-31 07:08:03 +0200

    Container Block.

--- assets/js/blocks/section/index.asset.php
+++ assets/js/blocks/section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c2105f8a81faa37c87d6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '1f4ad2339d02641cc453');

8eb88b4e0697fad708867dd1bda21c809a138efa
EngCode <elampratorabdallah14@gmail.com>
2023-01-20 02:37:20 +0200

    Logo Block Fixed.

--- assets/js/blocks/logo/index.asset.php
+++ assets/js/blocks/logo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '4625082248c80a68a105');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '1092659f8a05a9583449');

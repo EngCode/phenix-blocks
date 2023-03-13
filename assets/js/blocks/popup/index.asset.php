52d01267e8896824616fb7cd11987ebb9b487ed0
EngCode <elampratorabdallah14@gmail.com>
2023-03-13 18:32:24 +0200

    update modal.

--- assets/js/blocks/popup/index.asset.php
+++ assets/js/blocks/popup/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '05348a3e7642ce471fc2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '322ea59059eb6edee863');

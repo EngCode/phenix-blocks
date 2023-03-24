5d42049f79ce601d4c2baa72bd2d1a3923b06856
EngCode <elampratorabdallah14@gmail.com>
2023-03-24 20:00:37 +0200

    update.

--- assets/js/blocks/group/index.asset.php
+++ assets/js/blocks/group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f0d03813cd1fbd4a1fc6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e494d212991ad238421');

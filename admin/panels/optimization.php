e260d75fdd7649926dab6f5904287b4684a0c0d0
EngCode <elampratorabdallah14@gmail.com>
2023-12-20 15:55:23 +0200

    Loading Options Live Preview.

--- admin/panels/optimization.php
+++ admin/panels/optimization.php
@@ -137,3 +137,65 @@
     <!-- // Column -->
 </div>
 <!-- // Grid -->
+
+<!-- Scripts -->
+<script defer>
+    document.addEventListener('DOMContentLoaded', ready => {
+        //===> Loading Live Preview <===//
+        Phenix('#pds_loading_type').on("change", isChanged => {
+            //===> Get the Value <===//
+            let value = isChanged.target.value;
+
+            //===> Check if the Value is "image" <===//
+            if(value == "image") {
+                //===> Show the Image Controls <===//
+                Phenix(".loading-preview .loading-image").removeClass("hidden");
+                Phenix(".loading-preview .loading-code, #pds_loading_code").addClass("hidden");
+            } else {
+                //===> Show the Code Controls <===//
+                Phenix(".loading-preview .loading-image").addClass("hidden");
+                Phenix(".loading-preview .loading-code, #pds_loading_code").removeClass("hidden");
+            }
+        });
+
+        Phenix('#pds_loading_text').on("change", isChanged => {
+            //===> Get the Value <===//
+            let value = isChanged.target.value;
+            //===> Change the Preview <===//
+            document.querySelector(".loading-preview .loading-text").innerHTML = value;
+        });
+
+        Phenix('#pds_loading_image').on("change", isChanged => {
+            //===> Get the Value <===//
+            let value = isChanged.target.value;
+            //===> Change the Preview <===//
+            document.querySelector(".loading-preview .loading-image").setAttribute("src", value);
+        });
+
+        Phenix('#pds_loading_code textarea').on("change", isChanged => {
+            //===> Get the Value <===//
+            let value = isChanged.target.value;
+            //===> Change the Preview <===//
+            document.querySelector(".loading-preview .loading-code").innerHTML = value;
+        });
+
+        Phenix('#pds_loading_background input').on("change", isChanged => {
+            //===> Get the Value <===//
+            let value = isChanged.target.value;
+            //===> Change the Preview <===//
+            document.querySelector(".loading-preview").style.background = value;
+            document.querySelector("#pds_loading_background .btn").style.background = value;
+        });
+
+        Phenix('#pds_loading_showText').on("change", isChanged => {
+            //===> Get the Value <===//
+            let checked = isChanged.target.checked;
+            //===> Change the Preview <===//
+            if (checked === false) {
+                document.querySelector(".loading-preview .loading-text").classList.add("hidden");
+            } else {
+                document.querySelector(".loading-preview .loading-text").classList.remove("hidden");
+            }
+        });
+    });
+</script>

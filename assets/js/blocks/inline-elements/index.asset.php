3df2204555a50004d2a20a8f788bb0f66e7c411d
EngCode <elampratorabdallah14@gmail.com>
2023-12-21 12:55:37 +0200

    fix shrinking bug, core css.

--- assets/js/blocks/inline-elements/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e92a4c1888a2a0c02ce8');

17c7235289c686934bfb5a20f0102427f76a66cc
EngCode <elampratorabdallah14@gmail.com>
2024-01-29 06:49:34 +0200

    update dashboard ui.

--- admin/pds-dashboard.php
+++ admin/pds-dashboard.php
@@ -17,8 +17,8 @@
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="bg-grade-purple pd-25 pd-mg-30 tx-align-center radius-md bg-grade-180">
                     <a href="https://phenixthemes.notion.site/Phenix-Blocks-Developers-b7ae033ce7484e8f98209d7587a94792" target="_blank" class="display-block">
-                        <h3 class="color-white fs-18 tx-uppercase">Editors Guide-lines</h3>
-                        <p class="color-white">Getting start with the User Guidelines for Content Editors and Marketers.</p>
+                        <h3 class="color-white fs-18 tx-uppercase"><?php echo __("Editors Guide-lines", "pds-blocks"); ?></h3>
+                        <p class="color-white"><?php echo __("Getting start with the User Guidelines for Content Editors and Marketers.", "pds-blocks"); ?></p>
                     </a>
                 </div>
             </div>
@@ -26,8 +26,8 @@
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="bg-grade-primary pd-25 pd-mg-30 tx-align-center radius-md bg-grade-180">
                     <a href="https://phenixthemes.notion.site/Phenix-Design-System-efcfa4d3839946989a4f94ee5e0480c3" target="_blank" class="display-block">
-                        <h3 class="color-white fs-18 tx-uppercase">Developers Docs</h3>
-                        <p class="color-white">the Developer Docs to learn more about the development tools in Phenix.</p>
+                        <h3 class="color-white fs-18 tx-uppercase"><?php echo __("Developers Docs", "pds-blocks"); ?></h3>
+                        <p class="color-white"><?php echo __("the Developer Docs to learn more about the development tools in Phenix.", "pds-blocks"); ?></p>
                     </a>
                 </div>
             </div>
@@ -35,8 +35,8 @@
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="bg-grade-secondary pd-25 pd-mg-30 tx-align-center radius-md bg-grade-180">
                     <a href="https://phenixthemes.com" target="_blank" class="display-block">
-                        <h3 class="color-dark fs-18 tx-uppercase">More Themes!</h3>
-                        <p class="color-dark">a new themes arrived on Phenix give us a visit to update your theme</p>
+                        <h3 class="color-dark fs-18 tx-uppercase"><?php echo __("More Themes!", "pds-blocks"); ?></h3>
+                        <p class="color-dark"><?php echo __("a new themes arrived on Phenix give us a visit to update your theme", "pds-blocks"); ?></p>
                     </a>
                 </div>
             </div>
@@ -52,7 +52,7 @@
                     <!-- PDB Logo -->
                     <img src="<?php echo $images_folder;?>/px-logo/px-blocks-icon.svg" alt="" width="100" class="mb-20" />
                     <!-- About -->
-                    <p class="color-gray fs-14">With Phenix Blocks, Developers and Content Creators no longer have to start from scratch or spend excessive time crafting individual components. Instead, they can leverage the extensive library of Phenix Blocks to assemble their web pages effortlessly.</p>
+                    <p class="color-gray fs-14"><?php echo __("With Phenix Blocks, Developers and Content Creators no longer have to start from scratch or spend excessive time crafting individual components. Instead, they can leverage the extensive library of Phenix Blocks to assemble their web pages effortlessly.","pds-blocks"); ?></p>
                     <!-- AR Logo -->
                     <img src="<?php echo $images_folder;?>/dashboard/sig.svg" alt="" width="250" />
                 </div>

cfe8ec328215a5e5933e9a6f4bc9d4b9661d943e
EngCode <elampratorabdallah14@gmail.com>
2024-02-01 07:06:29 +0200

    improve All-In-One Options Renderer.

--- assets/js/blocks/grid-row/index.asset.php
+++ assets/js/blocks/grid-row/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e816ce23170c89a6123b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5aa4e21eb87b624c7cbe');

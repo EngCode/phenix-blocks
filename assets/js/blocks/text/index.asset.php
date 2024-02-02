13e57c834f05a3b9f69aed7bf5518bf15500a25e
EngCode <elampratorabdallah14@gmail.com>
2024-02-02 19:28:42 +0200

    add Convert to Link for Groups and Text Elements.

--- assets/js/blocks/text/index.asset.php
+++ assets/js/blocks/text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c4ffdbb6b14fa5ceeba3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5a2510f60214ed151a3a');

cd1eacc59dfaa2d1a9ffba3b5d83c256a4644aea
EngCode <elampratorabdallah14@gmail.com>
2024-02-03 10:51:10 +0200

    improve accessibility.

--- assets/js/blocks/text/index.asset.php
+++ assets/js/blocks/text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f5c4c5e10233cb75e056');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a8b9ff36819b743ab6ac');

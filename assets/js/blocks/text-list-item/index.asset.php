62c70db4164285c45f79dfee762633e42c0478e4
EngCode <elampratorabdallah14@gmail.com>
2024-02-03 10:51:00 +0200

    improve accessibility.

--- assets/js/blocks/text-list-item/index.asset.php
+++ assets/js/blocks/text-list-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '18039a8c732a2645e5f3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ab6eabc27fc983169bc2');

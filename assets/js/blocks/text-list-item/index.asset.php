1420ad9ab95d0eae249f37300c991382b9278efb
EngCode <elampratorabdallah14@gmail.com>
2024-02-04 15:01:19 +0200

    fix list item hidden from inserter.

--- assets/js/blocks/text-list-item/index.asset.php
+++ assets/js/blocks/text-list-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '75a040dccdba325db165');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '00a9187c7dcbe86d3996');

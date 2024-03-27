50c1f1dcb74921a0d7473d0c98738d6fd8ce7991
EngCode <elampratorabdallah14@gmail.com>
2024-03-27 09:42:53 +0200

    Fix duplicated meta.

--- src/scripts/integration/wordpress.ts
+++ src/scripts/integration/wordpress.ts
@@ -56,6 +56,7 @@ Phenix(window).on("load", (loaded) => {
         document.body.setAttribute('itemscope', "");
         document.body.setAttribute('itemtype', "https://schema.org/WebPage");
         let pds_keywords = `${document.title}, HTML, Phenix, Abdullah, Ramadan, Web, Designer, Developer, Design System, WordPress, phenixthemes.com`;
+        let pds_meta_description = document.querySelector('.entry-content p:first-of-type')?.textContent.substring(0, 160);
         if (document.location.href.includes('phenixthemes.com')) {
             pds_keywords += `,شركة برمجة, تصميم مواقع, شركة تصميم مواقع, تصميم موقع الكتروني, تصميم مواقع الكترونية, شركات تصميم مواقع الكترونية, تصميم واجهة مستخدم, تطوير موقع ووردبريس, شركات تطوير مواقع الكترونية, شركات تصميم المواقع الالكترونية, تصميم مواقع الشركات, افضل شركة لتصميم المواقع, افضل شركات تصميم متاجر الكترونية, شركات تطوير المواقع الالكترونية, افضل شركات تصميم منصات الكترونية, افضل شركة برمجة منصات, شركات لعمل مواقع الانترنت, شركة برمجة وتصميم مواقع, أسعار تصميم المواقع, شركة تصميم مواقع ويب`
         }
@@ -64,7 +65,7 @@ Phenix(window).on("load", (loaded) => {
         if(!document.querySelector('h1')) Phenix('.main-header').insert('append', `<h1 class="hidden">${document.title}</h1>`);
 
         //====> S.E.O : Missing Meta <====//
-        if (!document.head.querySelector('meta[name="description"]')) Phenix(document.head).insert('append', `<meta name="description" content="this is ${document.title} website with no proper description.">`);
+        if (!document.head.querySelector('meta[name="description"]')) Phenix(document.head).insert('append', `<meta name="description" content="${pds_meta_description}">`);
         if (!document.head.querySelector('meta[name="keywords"]')) Phenix(document.head).insert('append', `<meta name="keywords" content="${pds_keywords}">`);
 
         //====> Links do not have a discernible name <====//

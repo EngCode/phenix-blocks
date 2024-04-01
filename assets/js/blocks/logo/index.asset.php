4fcdf3dc5e17fd4ea4591aa2f515d2b351aa3406
EngCode <elampratorabdallah14@gmail.com>
2024-04-01 22:11:51 +0200

    add Support 2K and 4K for Screens.

--- assets/js/blocks/logo/index.asset.php
+++ assets/js/blocks/logo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c759d46ee303b6965223');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1c083f55e368b3cdbb23');
--- src/blocks/logo/index.js
+++ src/blocks/logo/index.js
@@ -34,7 +34,7 @@ registerBlockType(metadata, {
         //===> Render <===//
         return (
             <a { ...blockProps }>
-                <img src={attributes.logo} className={attributes.responsive ? 'hidden-md-down' : ''} alt={blockProps.title} style={{ "maxHeight": `${Phenix(document).toREM(attributes.size)}` }} width={parseInt(attributes.width) ? parseInt(attributes.width) : null} height={Phenix(document).toREM(attributes.size)} />
+                <img src={attributes.logo} className={attributes.responsive ? 'hidden-md-down' : ''} alt={blockProps.title} style={{ "maxHeight": `${Phenix(document).toREM(attributes.size)}` }} width={Phenix(document).toREM(attributes.width) ? Phenix(document).toREM(attributes.width) : null} height={Phenix(document).toREM(attributes.size)} />
                 {attributes.responsive ?  <img src={attributes.use_fevicon ? attributes.fevicon : attributes.mobile_logo} className='hidden-lg-up' alt={blockProps.title} width={Phenix(document).toREM(attributes.size)} height={Phenix(document).toREM(attributes.size)} /> : null}
             </a>
         );

25d67a69d743d583e052444d0439ab04fdfbc974
EngCode <elampratorabdallah14@gmail.com>
2024-04-03 14:14:56 +0200

    WP 6.5 Update Fixes.

--- assets/js/blocks/logical-block/index.asset.php
+++ assets/js/blocks/logical-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '85dfaa3db4b6a1d49814');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8cfee4d087160ee91cc8');

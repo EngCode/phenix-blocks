563b68a57ab0b0734fe4388473bc31ac0785d07f
EngCode <elampratorabdallah14@gmail.com>
2024-04-06 17:05:18 +0200

    Fix Loading Media Uploader opens multiple times.

--- admin/panels/optimization.php
+++ admin/panels/optimization.php
@@ -161,28 +161,6 @@
 <!-- Scripts -->
 <script defer>
     document.addEventListener('DOMContentLoaded', ready => {
-        //===> WP Media Uploader <===//
-        Phenix(".px-custom-uploader .uploader-btn").on("click", isClicked => {
-            //===> Prevent Default <===//
-            isClicked.preventDefault();
-            let input = Phenix(isClicked.target).next(".uploader-input");
-            
-            //===> Open Media Uploader <===//
-            var image = wp.media({
-                title: "Upload Image",
-                multiple: false
-            }).open().on("select", isSelect => {
-                //===> Get the Image URL <===//
-                var uploaded_image = image.state().get("selection").first();
-
-                //===> Set the URL to the Input <===//
-                input.value = uploaded_image.toJSON().url;
-
-                //===> Loading Live Preview <===//
-                document.querySelector(".loading-preview .loading-image").setAttribute("src", uploaded_image.toJSON().url);
-            });
-        });
-
         //===> Loading Live Preview <===//
         Phenix('#pds_loading_type').on("change", isChanged => {
             //===> Get the Value <===//

d5bd4cc1ea968b1aded55b6888685862d09aa7b6
EngCode <elampratorabdallah14@gmail.com>
2024-04-27 18:19:42 +0300

    icon element animations set fix.

--- assets/js/blocks/icon-element/index.asset.php
+++ assets/js/blocks/icon-element/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9d8a9c5112afa6d191f3');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4f79e875c48add40f514');

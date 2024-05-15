c23937464f4ac1d62a4f5176b95f61fdb6834259
EngCode <elampratorabdallah14@gmail.com>
2024-05-15 15:13:45 +0300

    Update v1.0.9

--- inc/pds-updater.php
+++ inc/pds-updater.php
@@ -13,7 +13,7 @@ class PDS_GitHub_Updater {
 	//===> Define Data <===//
 	var $config;
 	var $missing_config;
-	const VERSION = "1.0.8";
+	const VERSION = "1.0.9";
 	private $github_data;
 
 	//=====> Constructor <====//

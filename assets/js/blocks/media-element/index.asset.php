4762718af2a6f75035ac493187097eca3622718c
EngCode <elampratorabdallah@yahoo.com>
2025-01-04 14:54:49 +0200

    Fix Media Settings for Video.

--- assets/js/blocks/media-element/index.asset.php
+++ assets/js/blocks/media-element/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2ea5b10b5849637a33fe');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f2dd9fd6f46d5ec25eb2');

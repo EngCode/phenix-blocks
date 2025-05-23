db334883c940085a67613d4a6df6f62faaf5fd73
EngCode <elampratorabdallah@yahoo.com>
2025-05-23 08:25:34 +0300

    Docs custom theme.

---
+++ docs-src/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+// .vitepress/theme/index.ts
+import { h } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout)
+  }
+}
